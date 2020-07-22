c9426102d322d25932aa81ebd0374d9eead71210
Gleb Garunchik <garunchikgleb@gmail.com>
2020-07-22 22:59:48 +0300

    added waiter because of api fail

--- src/test/java/tests/VacanciesTest.java
+++ src/test/java/tests/VacanciesTest.java
@@ -4,22 +4,22 @@ import adapters.hh.VacancyAdapter;
 import models.hh.VacanciesList;
 import org.testng.annotations.Test;
 
-import static resources.constants.AreaConstants.BELARUS_AREA;
-import static resources.constants.CurrConstants.USD_ID;
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+import static resources.constants.AreaConstants.*;
+import static resources.constants.CurrConstants.*;
 import static resources.constants.ExperienceConstants.*;
-import static resources.constants.VacancyNameConstants.QA_AUTO;
-import static resources.constants.VacancyNameConstants.QA_MANUAL;
+import static resources.constants.VacancyNameConstants.*;
 
 public class VacanciesTest {
 
     @Test
-    public void getVacancies() {
+    public void getVacancies() throws InterruptedException {
         VacanciesList vacanciesList = new VacancyAdapter().get(QA_AUTO, BELARUS_AREA, JUNIOR_EXP);
-        System.out.println("Cредняя ЗП AUTO: " + vacanciesList.getItems().get(0).getSalary().getAverageInCurrency(vacanciesList, USD_ID));
+        System.out.println("Cредняя ЗП AUTO c опытом " + JUNIOR_EXP + ":" + vacanciesList.getItems().get(0).getSalary().getAverageInCurrency(vacanciesList, USD_ID));
         System.out.println(vacanciesList);
+        Thread.sleep(8000);
         VacanciesList vacanciesList2 = new VacancyAdapter().get(QA_MANUAL, BELARUS_AREA, JUNIOR_EXP);
-        System.out.println("Cредняя ЗП MANUAL: " + vacanciesList2.getItems().get(0).getSalary().getAverageInCurrency(vacanciesList2, USD_ID));
+        System.out.println("Cредняя ЗП MANUAL c опытом" + JUNIOR_EXP + ":" + vacanciesList2.getItems().get(0).getSalary().getAverageInCurrency(vacanciesList2, USD_ID));
         System.out.println(vacanciesList2);
-
     }
 }
